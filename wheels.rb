38a8691ca9c04926d3f415b16bb3cc1065ededc5
Chris Thorn <thorncp@gmail.com>
2013-04-16 17:46:21 -0800

    Route struct

--- wheels.rb
+++ wheels.rb
@@ -5,26 +5,29 @@ require 'open-uri'
 require 'json'
 
 Wheels = Struct.new(:routes, :stop_schedules)
+Route = Struct.new(:id, :name, :stops, :geometry, :vehicles)
 
 wheels = Wheels.new
 
 doc = Nokogiri::HTML(open('http://bustracker.muni.org/InfoPoint/noscript.aspx'))
-wheels.routes = doc.css('.routeNameListEntry').map {|l| {:id => l['routeid'], :name => l.content}}
+
+wheels.routes = doc.css('.routeNameListEntry').map { |l| Route.new(l['routeid'], l.content) }
+
 wheels.routes.each do |r|
-  doc = Nokogiri::XML(open("http://bustracker.muni.org/InfoPoint/map/GetRouteXml.ashx?routeNumber=#{r[:id]}"))
+  doc = Nokogiri::XML(open("http://bustracker.muni.org/InfoPoint/map/GetRouteXml.ashx?routeNumber=#{r.id}"))
   stops = doc.css('stop').map do |s|
     stop = {:id => s['html'], :name => s['label'], :lat => s['lat'], :lng => s['lng']}
   end
-  r[:stops] = stops
+  r.stops = stops
   trace_url = doc.css('info').first['trace_kml_url']
   doc = Nokogiri::XML(open(trace_url))
-  r[:geometry] = doc.css('coordinates').map do |line|
+  r.geometry = doc.css('coordinates').map do |line|
     segments = line.text.split(' ').map do |p|
       c = p.split(',')
       {:lat => c[1], :lng => c[0]}
     end
   end
-  doc = Nokogiri::XML(open("http://bustracker.muni.org/InfoPoint/map/GetVehicleXml.ashx?RouteId=#{r[:id]}"))
+  doc = Nokogiri::XML(open("http://bustracker.muni.org/InfoPoint/map/GetVehicleXml.ashx?RouteId=#{r.id}"))
   vehicles = doc.css('vehicle').map do |v|
     doc = Nokogiri::HTML(open("http://bustracker.muni.org/InfoPoint/map/GetVehicleHtml.ashx?vehicleid=#{v['name']}"))
     vehicle = {:id => v['name'], :lat => v['lat'], :lng => v['lng'] }
@@ -34,9 +37,9 @@ wheels.routes.each do |r|
     end
     vehicle
   end
-  r[:vehicles] = vehicles
+  r.vehicles = vehicles
 end
-wheels.stop_schedules = wheels.routes.map {|r| r[:stops].map {|s| s[:id]}}.flatten.uniq.map do |s|
+wheels.stop_schedules = wheels.routes.map {|r| r.stops.map {|s| s[:id]}}.flatten.uniq.map do |s|
   doc = Nokogiri::HTML(open("http://bustracker.muni.org/InfoPoint/map/GetStopHtml.ashx?stopid=#{s}"))
   sched = {:stop_id => s}
   sched[:departures] = doc.css('.oddDepartureGroup,.evenDepartureGroup').map do |d|

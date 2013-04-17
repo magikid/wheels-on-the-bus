d7701342be0dc4d8a3b6b8c149ad79c87fce7c6b
Chris Thorn <thorncp@gmail.com>
2013-04-16 17:40:31 -0800

    Wheels struct

--- wheels.rb
+++ wheels.rb
@@ -4,10 +4,13 @@ require 'nokogiri'
 require 'open-uri'
 require 'json'
 
-wheels = {:routes => [], :stop_schedules => []}
+Wheels = Struct.new(:routes, :stop_schedules)
+
+wheels = Wheels.new
+
 doc = Nokogiri::HTML(open('http://bustracker.muni.org/InfoPoint/noscript.aspx'))
-wheels[:routes] = doc.css('.routeNameListEntry').map {|l| {:id => l['routeid'], :name => l.content}}
-wheels[:routes].each do |r|
+wheels.routes = doc.css('.routeNameListEntry').map {|l| {:id => l['routeid'], :name => l.content}}
+wheels.routes.each do |r|
   doc = Nokogiri::XML(open("http://bustracker.muni.org/InfoPoint/map/GetRouteXml.ashx?routeNumber=#{r[:id]}"))
   stops = doc.css('stop').map do |s|
     stop = {:id => s['html'], :name => s['label'], :lat => s['lat'], :lng => s['lng']}
@@ -33,7 +36,7 @@ wheels[:routes].each do |r|
   end
   r[:vehicles] = vehicles
 end
-wheels[:stop_schedules] = wheels[:routes].map {|r| r[:stops].map {|s| s[:id]}}.flatten.uniq.map do |s|
+wheels.stop_schedules = wheels.routes.map {|r| r[:stops].map {|s| s[:id]}}.flatten.uniq.map do |s|
   doc = Nokogiri::HTML(open("http://bustracker.muni.org/InfoPoint/map/GetStopHtml.ashx?stopid=#{s}"))
   sched = {:stop_id => s}
   sched[:departures] = doc.css('.oddDepartureGroup,.evenDepartureGroup').map do |d|
@@ -42,4 +45,5 @@ wheels[:stop_schedules] = wheels[:routes].map {|r| r[:stops].map {|s| s[:id]}}.f
   end
   sched
 end
-puts JSON.generate(wheels)
+
+puts wheels.to_json

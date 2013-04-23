cd84497ae6c93d4465c31cd5964cd06c0b2f1649
Chris W Jones <chris@christopherjones.us>
2013-04-23 04:07:47 -0800

    Got the basic structure of the file down.  For some reason, it only works on routes 7 and 14 atm.  WIP.

---
+++ stop_times.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/env ruby
+
+#foreign keys
+#trip_id (it's the route id)
+#arrival_time
+#departure_time
+#stop_id
+#stop_sequence
+
+
+require 'nokogiri'
+require 'open-uri'
+require 'json'
+
+wheels = {:routes => [], :stop_schedules => []}
+doc = Nokogiri::HTML(open('http://bustracker.muni.org/InfoPoint/noscript.aspx'))
+wheels[:routes] = doc.css('.routeNameListEntry').map {|l| {:id => l['routeid'], :name => l.content}}
+
+stops_info = []
+
+wheels[:routes].each do |r|
+  puts "Route: #{r[:id]}"
+  stop_info = Nokogiri::XML(open("http://bustracker.muni.org/InfoPoint/map/GetRouteXml.ashx?routeNumber=#{r[:id]}"))
+  schedule_info = Nokogiri::HTML(open("http://www.muni.org/Departments/transit/PeopleMover/Route%202012%20Schedules%20HTML/#{r[:id].rjust(3, '0')}.htm"))
+  stops = stop_info.css('stop').map do |s|
+    stop = {:id => s['html'], :name => s['label']}
+  end
+  
+  main_stops = schedule_info.css('table tr')[3].css('td').map {|s| s.text.strip.gsub(/&/, 'and') }
+  puts "Number stops: #{stops.length}"
+  puts "Number main stops: #{main_stops.length}"
+  main_stops.each do |m|
+    stops.each do |n|
+      if n[:name].include? m
+        stops_info.push({
+          :trip_id => r[:id],
+          :stop_id => n[:id],
+        })
+      end
+    end
+  end
+  
+end
+
+
+
+puts JSON.generate(stops_info)

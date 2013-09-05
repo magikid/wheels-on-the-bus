a884dbe1fe2dd29fd10421a243dd61d99c99901a
Chris Jones <chris@christopherjones.us>
2013-09-05 15:37:36 -0800

    Moved the build stuff from stop_times to Rakefile

--- stop_times.rb
+++ stop_times.rb
@@ -74,22 +74,3 @@ class Busses
     return "0"
   end
 end
-
-wheels = {:routes => [], :stop_schedules => []}
-doc = Nokogiri::HTML(open('http://bustracker.muni.org/InfoPoint/noscript.aspx'))
-wheels[:routes] = doc.css('.routeNameListEntry').map {|l| {:id => l['routeid'], :name => l.content}}
-
-stop_times = []
-wheels[:routes].each{|r|
-  b = Busses.new(r[:id])
-  stop_times.push(b.read_stop_times)
-}
-
-CSV.open("CSVs/stop_times.txt", "wb"){|csv|
-  csv << ["trip_id", "arrival_time", "departure_time", "stop_id", "stop_sequence"]
-  stop_times.each{|b|
-    b.map{|c|
-      csv << c.values
-    }
-  }
-}

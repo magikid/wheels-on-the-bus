ab6d9b2c91a91057df008623aa18fa8b76612656
Chris Jones <chris@christopherjones.us>
2014-04-09 22:54:57 +0000

    Starting on the stop class

---
+++ stop.rb
@@ -0,0 +1,7 @@
+##
+# This class is used to describe the stops
+# in the Google Transit Feed Spec.
+#
+class Stop
+
+end
---
+++ test/stop_test.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+require './stop.rb'
+
+describe Stop, 'testing Stop class' do
+  before do
+    @stop = Stop.new
+  end
+end

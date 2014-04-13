db6054d03e21eff7bcc938c424e7d963f95f7d85
Chris Jones <chris@christopherjones.us>
2014-04-13 14:23:57 +0000

    Fixed some rubocop errors in route

--- route.rb
+++ route.rb
@@ -1,8 +1,11 @@
+##
+# This class describes a route for GTFS
+#
 class Route
   attr_accessor :route_type
   attr_reader :route_id, :route_short_name, :route_long_name
 
-  def initialize(route_id, route_short_name, route_long_name, route_type=3)
+  def initialize(route_id, route_short_name, route_long_name, route_type = 3)
     @route_id = route_id
     @route_short_name = route_short_name
     @route_long_name = route_long_name

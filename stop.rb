21b25ed13e7ad27c38f26578fc7056e99690158e
Chris Jones <chris@christopherjones.us>
2014-04-13 12:43:14 +0000

    Constructor done

--- stop.rb
+++ stop.rb
@@ -3,5 +3,10 @@
 # in the Google Transit Feed Spec.
 #
 class Stop
-
+  def initialize(stop_id, stop_name, stop_lat, stop_lon)
+    @stop_name = stop_name
+    @stop_id = stop_id
+    @stop_lat = stop_lat
+    @stop_lon = stop_lon
+  end
 end
--- test/stop_test.rb
+++ test/stop_test.rb
@@ -3,6 +3,10 @@ require './stop.rb'
 
 describe Stop, 'testing Stop class' do
   before do
-    @stop = Stop.new
+    @stop = Stop.new(0, "", 0, 0)
+  end
+
+  it 'returns a stop object' do
+    @stop.must_be_kind_of(Stop)
   end
 end
